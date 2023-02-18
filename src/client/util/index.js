2edddfe03237e85fc7920ef00e9db579dd0496df
cj dagostino <cjd.dagostino@gmail.com>
2023-02-18 10:25:43 -0500

    #4 calculateWins works

--- src/client/util/index.js
+++ src/client/util/index.js
@@ -1,12 +1,3 @@
-const grid = Array.from({ length : 42 }, (_, i) => { color: '' })
-let s = `
-0  1  2  3  4  5  6
-7  8  9  10 11 12 13
-14 15 16 17 18 19 20
-21 22 23 24 25 26 27
-28 29 30 31 32 33 34
-35 36 37 38 39 40 41
-`
 const getMatricies = (index) => {
   let matrix = []
   let m = []
@@ -71,6 +62,41 @@ const getMatricies = (index) => {
   m = []
   return matrix;
 }
+
+const calculateWins = (grid, index) => {
+  matricies = getMatricies(index)
+  return matricies.reduce((overall, arr) => {
+    if (overall.won) {
+      return overall;
+    }
+    arr = arr.map(i => grid[i].color)
+    let currentColor = arr[0]
+    arr = arr.slice(1)
+    let maxSameColor = arr.reduce((acc, color) => {
+      if (acc === 4) return 4;
+      if (color && color === currentColor) {
+        return acc + 1
+      }
+      currentColor = color
+      return 1
+    }, 1)
+    if (maxSameColor === 4) {
+      overall.won = true
+      overall.winningColor = currentColor
+    }
+    return overall
+    
+  }, { won: false, winningColor: ''})
+}
+
+/*
+0  1  2  3  4  5  6
+7  8  9  10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30 31 32 33 34
+35 36 37 38 39 40 41
+*/
 // console.log(6,getMatricies(6))
 // console.log(9,getMatricies(9))
 // console.log(13,getMatricies(13))
@@ -80,7 +106,24 @@ const getMatricies = (index) => {
 // console.log(14,getMatricies(14))
 // console.log(36,getMatricies(36))
 // console.log(27,getMatricies(27))
+// let grid = Array.from({ length : 42 }, (_, i) => { color: '' })
+// // HORIZONTAL
+// grid = Array.from({ length : 42 }, (_, i) => ({ color: '' }))
+// for (let i = 37; i < 41; i++) {
+//     grid[i].color = 'red';
+//   }
+//   console.log(calculateWins(grid, 38))
+//   // DIAGONAL /
+//   grid = Array.from({ length : 42 }, (_, i) => ({ color: '' }))
+//   for (let i = 11; i < 30; i+=6) {
+//     grid[i].color = 'red';
+//   }
+//   console.log(calculateWins(grid, 11))
+//   // DIAGONAL \
+//   grid = Array.from({ length : 42 }, (_, i) => ({ color: '' }))
+//   for (let i = 14; i < 39; i+=8) {
+//     grid[i].color = 'red';
+//   }
+//   console.log(calculateWins(grid, 30))
 
-const calculateWins = (grid, index) => {
-  matricies = getMatricies()
-}
+export default calculateWins
